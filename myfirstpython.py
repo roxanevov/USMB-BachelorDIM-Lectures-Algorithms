bd46774b1301800c36f0a3928ee9c7cd7ecc422c
Alben <benoit.alexandre.vision@gmail.com>
2017-08-14 17:20:52 +0200

    test bug introduction

--- myfirstpython.py
+++ myfirstpython.py
@@ -1,6 +1,12 @@
 #python trials script
 print('basic loop example...')
-
+a=0
 for i in xrange(10):
     a=a+i
 print('a='+str(a))
+
+if a>10:
+    print('a>10')
+elif a>5:
+    print('a>5... never displayed...')
+
