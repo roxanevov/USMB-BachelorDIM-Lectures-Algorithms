988b30a52f0131fc123d5df3e9fcaae80e06ccd4
Alben <benoit.alexandre.vision@gmail.com>
2017-08-15 13:40:40 +0200

    added many new code duplicates

--- myfirstpython.py
+++ myfirstpython.py
@@ -5,10 +5,17 @@ for i in xrange(10):
     a=a+i
 print('a='+str(a))
 
-if a>10:
+#redundant parenthesis
+if ((a>10)):
     print('a>10')
 elif a>5:
     print('a>5... never displayed...')
+#FIXME test if fixme is found
+
+#collapsing if statementsmistake
+if a>10:
+    if a>20:
+        print('a est high')
 a=0
 for i in xrange(10):
     a=a+i
